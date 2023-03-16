c8ee33f89cfc2546fb3264ef5830c998d30bd23a
gunaseelan321 <112534881+gunaseelan321@users.noreply.github.com>
2023-03-16 18:47:46 +0530

    added yasdi protocol docker

--- yasdi_ansible.sh
+++ yasdi_ansible.sh
@@ -8,4 +8,12 @@ sudo docker run -itd --name myrabbitmq -p 5672:5672 -p 15672:15672 -p 1883:1883 
 
 docker run --name node-red3.0.2 --restart unless-stopped -p 1880:1880 -itd  nodered/node-red:3.0.2
 
+cd /home/iplon/Documents/yasdi2mqtt_tar.gz
+
+tar -xvf yasdi2mqtt_tar.gz
+
+docker-compose up -d
+
+
+
 
