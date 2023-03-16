94b83871a6550ff21c5c14cb7a7ffb37a0a43458
gunaseelan321 <112534881+gunaseelan321@users.noreply.github.com>
2023-03-16 18:33:50 +0530

    docker images added

---
+++ yasdi_ansible.sh
@@ -0,0 +1,11 @@
+cd /home/iplon/repos/
+
+
+
+
+
+sudo docker run -itd --name myrabbitmq -p 5672:5672 -p 15672:15672 -p 1883:1883 -p 15675:15675 rabbitmq:3-management
+
+docker run --name node-red3.0.2 --restart unless-stopped -p 1880:1880 -itd  nodered/node-red:3.0.2
+
+
